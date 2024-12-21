45bdda83e218a7995d50973d2afc9bc42296704e
Marcelo Arias <hello@marceloarias.com>
2024-12-21 09:28:44 -0500

    Integrate generate_image function

--- platzi-vision-api/app/api/chat.py
+++ platzi-vision-api/app/api/chat.py
@@ -30,7 +30,7 @@ def chat():
         formatted_messages = [
             {
                 "role": "system",
-                "content": "Eres un asistente llamado PlatziVision. Responde a las preguntas de los usuarios con claridad."
+                "content": "Eres un asistente llamado PlatziVision. Responde a las preguntas de los usuarios sobre Platzi con claridad. Puedes generar imágenes en DALL-E 3. También puedes analizar imágenes que el usuario te envía."
             }
         ]
 
@@ -59,21 +59,97 @@ def chat():
         
         client = OpenAI()
 
-        response = client.chat.completions.create(
-            model="gpt-4o-mini",
-            messages=formatted_messages,
-            stream=True
-        )
+        tools = [
+            {
+                "type": "function",
+                "function": {
+                    "name": "generate_image",
+                    "description": "Cuando el usuario lo solicite, genera una imagen",
+                    "parameters": {
+                        "type": "object",
+                        "properties": {
+                            "prompt": {
+                                "type": "string",
+                                "description": "El prompt que generará la imagen"
+                            },
+                            "quality": {
+                                "type": "string",
+                                "description": "Calidad de la imagen. Puede ser 'hd' o 'standard'"
+                            }
+                        }
+                    }
+                }
+            }
+        ]
 
         def generate():
-            for chunk in response:
-                if chunk.choices[0].delta.content:
-                    yield f"data: {json.dumps({ 'content': chunk.choices[0].delta.content, 'status': 'streaming' })}\n\n"
+            accumulated_args = ""
+            response = None
+            current_tool_call_id = None
 
-                if chunk.choices[0].finish_reason == "stop":
-                    yield f"data: {json.dumps({ 'status': 'done' })}\n\n"
-                    break
-        
+            while True:
+
+                if response is None:
+                    response = client.chat.completions.create(
+                        model="gpt-4o-mini",
+                        messages=formatted_messages,
+                        stream=True,
+                        tools=tools
+                    )
+
+                for chunk in response:
+                    if chunk.choices[0].delta.content:
+                        yield f"data: {json.dumps({ 'content': chunk.choices[0].delta.content, 'status': 'streaming' })}\n\n"
+
+                    if chunk.choices[0].finish_reason == "stop":
+                        yield f"data: {json.dumps({ 'status': 'done' })}\n\n"
+                        break
+
+                    if chunk.choices[0].delta.tool_calls:
+                        tool_call = chunk.choices[0].delta.tool_calls[0]
+
+                        if tool_call.id and tool_call.function.name:
+                            current_tool_call_id = tool_call.id
+                        
+                        if hasattr(tool_call.function, 'arguments') and tool_call.function.arguments:
+                            accumulated_args += tool_call.function.arguments
+                            print(f"Argumentos generados: {tool_call.function.arguments}")
+
+                        if accumulated_args.strip().endswith('}'):
+                            try:
+                                print("Iniciando la llamada a la función")
+                                function_args = json.loads(accumulated_args)
+
+                                if 'prompt' in function_args:
+                                    yield f"data: {json.dumps({ 'status': 'generating_image' })}"
+
+                                    image_url = generate_image(prompt=function_args["prompt"], quality=function_args["quality"])
+
+                                    formatted_messages.append({
+                                        "role": "assistant",
+                                        "content": None,
+                                        "tool_calls": [{
+                                            "id": current_tool_call_id,
+                                            "function": {
+                                                "name": "generated_image",
+                                                "arguments": accumulated_args
+                                            },
+                                            "type": "function"
+                                        }]
+                                    })
+
+                                    formatted_messages.append({
+                                        "role": "tool",
+                                        "tool_call_id": current_tool_call_id,
+                                        "content": image_url
+                                    })
+
+                                    response = None
+                                    break
+                            except:
+                                pass
+
+
         return Response(generate(), mimetype="text/event-stream")
 
     except Exception as e:
